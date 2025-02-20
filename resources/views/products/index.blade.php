0c189d7a1ff1a332f32c6e66b0742e56021f792c
Satyasy <satyavpandega@gmail.com>
2025-02-20 10:25:05 +0700

    change bg-color

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -9,14 +9,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body style="background: gray">
+<body style="background: lightgray">
 
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
-                    <h5 class="text-center"><a href="https://santrikoding.com">www.santrikoding.com</a></h5>
+                    <h3 class="text-center my-4">Dashboard Management Product</h3>
+                    <h5 class="text-center">SMK Telkom Sidoarjo</h5>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
@@ -74,7 +74,7 @@
 
     <script>
         //message with sweetalert
-        @if (session('success'))
+        @if(session('success'))
             Swal.fire({
                 icon: "success",
                 title: "BERHASIL",
@@ -82,7 +82,7 @@
                 showConfirmButton: false,
                 timer: 2000
             });
-        @elseif (session('error'))
+        @elseif(session('error'))
             Swal.fire({
                 icon: "error",
                 title: "GAGAL!",
